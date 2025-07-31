7d17385f5c61dedbfdf76327525dcd7dd0c5820d
radovand <rdragovic@khaoticen.com>
2025-07-31 14:33:44 +0200

    Changes: Added InputTypeDecimal; Parsing logic moved to input types; Allowed values moved from Constants to InputTypeDecimal;

--- Calculator.cs
+++ Calculator.cs
@@ -1,53 +1,29 @@
-﻿using CalculatorLib.Constants;
-using CalculatorLib.InputTypes;
-using CalculatorLib.Interfaces;
-using CalculatorLib.Services.Parsers;
+﻿using CalculatorLib.InputTypes;
+
 
 namespace CalculatorLib
 {
     public static class Calculator
     {
-        public static decimal Calculate(decimal num1, decimal num2, Operand operand)
+        public static decimal Calculate(InputTypeDecimal num1, InputTypeDecimal num2, InputTypeOperand inputTypeOperand)
         {
-            return operand.Apply(num1, num2);
+            return inputTypeOperand.Apply(num1, num2);
         }
 
         // INPUT VALUES LIMITATIONS
         public static decimal GetMaxDecimalValue()
         {
-            return  DecimalConstants.GetMaxDecimalValue();
+            return InputTypeDecimal.GetMaxValue();
         }
-        
+
         public static decimal GetMinDecimalValue()
         {
-            return DecimalConstants.GetMinDecimalValue();
+            return InputTypeDecimal.GetMinValue();
         }
 
         public static string GetOperandValues()
         {
-            return Operand.GetValues();
-        }
-
-        // PARSE
-        public static decimal ParseDecimal(string s)
-        {
-            return GetDecimalParser().Parse(s);
-        }
-    
-        public static Operand ParseOperand(string s)
-        {
-            return GetOperandParser().Parse(s);
-        }
-
-        // GET PARSERS
-        public static IParser<decimal> GetDecimalParser()
-        {
-            return new DecimalParser();
-        }
-        
-        public static IParser<Operand> GetOperandParser()
-        {
-            return new OperandParser();
+            return InputTypeOperand.GetValues();
         }
     }
 }
---
+++ InputTypes/InputTypeDecimal.cs
@@ -0,0 +1,27 @@
+﻿namespace CalculatorLib.InputTypes;
+
+public class InputTypeDecimal: IParsableInput<InputTypeDecimal>
+{
+    private InputTypeDecimal(decimal value) => Value = value;
+    
+    public decimal Value {
+        get;
+    }
+    
+    public static InputTypeDecimal Parse(string input)
+    {
+        if (!decimal.TryParse(input, out var number)) throw new FormatException($"'{input}' is not a valid number.");
+
+        return new InputTypeDecimal(number);
+    }
+    
+    public static decimal GetMaxValue()
+    {
+        return decimal.MaxValue;
+    }
+    
+    public static decimal GetMinValue()
+    {
+        return decimal.MinValue;
+    }
+}
